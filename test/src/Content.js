b45cbd7e0696cc47617a3df18408468bc889fd58
Peter Ball <peterwball1@gmail.com>
2024-03-10 18:09:54 +0000

    lesson5

--- test/src/Content.js
+++ test/src/Content.js
@@ -5,11 +5,25 @@ const Content = () => {
         return names[int];
     }
 
+    const handleClick = () => {
+        console.log('clicked');
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+    }
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClick2('Peter')}>Click me</button>
+            <button onClick={(e) => handleClick3(e)}>Click me</button>
         </main>
     );
 };
