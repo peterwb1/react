f3c314ed3e24ca2e11b6c948ae8e174af407fe4a
Peter Ball <peterwball1@gmail.com>
2024-03-10 16:31:56 +0000

    lesson2

--- test/src/App.js
+++ test/src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = ()=> { 
+    const names = ['bob','fatima','peter']
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to test.
+          Hello {handleNameChange()}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      
     </div>
   );
 }
